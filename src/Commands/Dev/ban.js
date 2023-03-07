b47139f9970ba1b5c52c233e04ff26f7c1ffbc99
Fantox <96835724+FantoX001@users.noreply.github.com>
2023-03-08 01:49:36 +0530

    Akeno

--- src/Commands/Dev/ban.js
+++ src/Commands/Dev/ban.js
@@ -8,6 +8,137 @@ module.exports = {
   usage:  `${prefa}ban @user`,
   react: "🎀",
   start: async (Akeno,m,{text,prefix,isBotAdmin,isAdmin,mentionByTag,metadata,pushName,isCreator,args,}) => {
-    //do something
+    if (!isCreator)
+      return Akeno.sendMessage(
+        m.from,
+        { text: "Sorry, only my *Mods* can use this command !" },
+        { quoted: m }
+      );
+
+      if (!text && !m.quoted) {
+        return Akeno.sendMessage(
+          m.from,
+          { text: `Please tag a user to *Ban*!` },
+          { quoted: m }
+        );
+      }
+  
+      if (m.quoted) {
+        var mentionedUser = m.quoted.sender;
+      } else {
+        var mentionedUser = mentionByTag[0];
+      }
+      //var mentionedUser = mentionByTag;
+      let GroupName = metadata.subject;
+      let banreason = args.join(" ");
+  
+      if (m.quoted && !args.join(" ")) {
+        banreason = "No reason provided";
+      }
+  
+      if (m.quoted && args.join(" ")) {
+        banreason = text;
+      }
+  
+      if (banreason.includes("@")) {
+        banreason = args.join(" ");
+      }
+  
+      if (banreason == undefined) {
+        banreason = "Broke the rules";
+      }
+      var ownerlist = global.owner;
+  
+      let userId = (await mentionedUser) || m.msg.contextInfo.participant;
+      try {
+        gsc
+          .findOne({ id: userId })
+          .then(async (user) => {
+            if (!user) {
+              if (
+                ownerlist.includes(`${mentionedUser.split("@")[0]}`)
+              )
+                return Akeno.sendMessage(
+                  m.from,
+                  {
+                    text: `@${
+                      mentionedUser.split("@")[0]
+                    } is a *Mod* and can't be banned !`,
+                    mentions: [mentionedUser],
+                  },
+                  { quoted: m }
+                );
+              await gsc.create({
+                id: userId,
+                ban: true,
+                reason: banreason,
+              });
+              return Akeno.sendMessage(
+                m.from,
+                {
+                  text: `@${
+                    mentionedUser.split("@")[0]
+                  } has been *Banned* Successfully by *${pushName}*\n\n *Reason*: ${banreason}`,
+                  mentions: [mentionedUser],
+                },
+                { quoted: m }
+              );
+            } else {
+              if (
+                ownerlist.includes(`${mentionedUser.split("@")[0]}`)
+              )
+                return Akeno.sendMessage(
+                  m.from,
+                  {
+                    text: `@${
+                      mentionedUser.split("@")[0]
+                    } is a *Mod* and can't be banned !`,
+                    mentions: [mentionedUser],
+                  },
+                  { quoted: m }
+                );
+              if (user.ban == "true")
+                return Akeno.sendMessage(
+                  m.from,
+                  {
+                    text: `@${mentionedUser.split("@")[0]} is already *Banned* !`,
+                    mentions: [mentionedUser],
+                  },
+                  { quoted: m }
+                );
+              await gsc.findOneAndUpdate(
+                { id: userId },
+                { $set: { ban: true, reason: banreason} },
+                { new: true }
+              );
+              return Akeno.sendMessage(
+                m.from,
+                {
+                  text: `@${
+                    mentionedUser.split("@")[0]
+                  } has been *Banned* Successfully by *${pushName}*\n\n *Reason*: ${banreason}`,
+                  mentions: [mentionedUser],
+                },
+                { quoted: m }
+              );
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            return Akeno.sendMessage(
+              m.from,
+              { text: `An internal error occurred while banning the user.` },
+              { quoted: m }
+            );
+          });
+      } catch (err) {
+        console.log(err);
+        return Akeno.sendMessage(
+          m.from,
+          { text: `An internal error occurred while banning the user.` },
+          { quoted: m }
+        );
+      }
+      
   }
 };
--- src/Commands/Dev/unban.js
+++ src/Commands/Dev/unban.js
@@ -1,11 +1,100 @@
+const { gsc } = require("../../Handlers/schema");
+
 module.exports = {
-    name: "unban",
-    alias: ["unban"],
-    desc: "unaban the tagged user",
-    react: "🧣",
-    usage:  `${prefa}unban @user`,
-    category: "Dev",
-    start: async(Akeno, m,{pushName,prefix}) => {
-        //Do Something
+  name: "unban",
+  alias: ["unban"],
+  desc: "unaban the tagged user",
+  react: "🧣",
+  usage: `${prefa}unban @user`,
+  category: "Dev",
+  start: async (
+    Akeno,
+    m,
+    {
+      text,
+      prefix,
+      isBotAdmin,
+      isAdmin,
+      mentionByTag,
+      pushName,
+      isCreator,
     }
-}
+  ) => {
+    if (!isCreator)
+      return Akeno.sendMessage(
+        m.from,
+        { text: "Sorry, only *Mods* can use this command !" },
+        { quoted: m }
+      );
+
+      if (!text && !m.quoted) {
+        return Akeno.sendMessage(
+          m.from,
+          { text: `Please tag a user to *Unban*!` },
+          { quoted: m }
+        );
+      } else if (m.quoted) {
+        var mentionedUser = m.quoted.sender;
+      } else {
+        var mentionedUser = mentionByTag[0];
+      }
+
+      let userId = (await mentionedUser) || m.msg.contextInfo.participant;
+    try {
+      gsc
+        .findOne({ id: userId })
+        .then(async (user) => {
+          if (!user) {
+            return Akeno.sendMessage(
+              m.from,
+              {
+                text: `@${mentionedUser.split("@")[0]} is not *Banned* !`,
+                mentions: [mentionedUser],
+              },
+              { quoted: m }
+            );
+          } else {
+            if (user.ban == "false")
+              return Akeno.sendMessage(
+                m.from,
+                {
+                  text: `@${mentionedUser.split("@")[0]} is not *Banned* !`,
+                  mentions: [mentionedUser],
+                },
+                { quoted: m }
+              );
+            await gsc.findOneAndUpdate(
+              { id: userId },
+              { ban: false },
+              { new: true }
+            );
+            return Akeno.sendMessage(
+              m.from,
+              {
+                text: `@${
+                  mentionedUser.split("@")[0]
+                } has been *Unbanned* Successfully! by *${pushName}*`,
+                mentions: [mentionedUser],
+              },
+              { quoted: m }
+            );
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          return Akeno.sendMessage(
+            m.from,
+            { text: `An internal error occurred while Unbanning the user.` },
+            { quoted: m }
+          );
+        });
+    } catch (err) {
+      console.log(err);
+      return Akeno.sendMessage(
+        m.from,
+        { text: `An internal error occurred while Unbanning the user.` },
+        { quoted: m }
+      );
+    }
+  },
+};
